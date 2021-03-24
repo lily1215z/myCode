a5f592fccd91f9166477c0019f92b3145f70e2bd
lily1215z <60219984+lily1215z@users.noreply.github.com>
2021-03-25 00:49:56 +0300

    Update script.js

--- бургер меню при адаптиве/script.js
+++ бургер меню при адаптиве/script.js
@@ -6,7 +6,7 @@ const menuBody = document.querySelector('.menu__body');
 if(iconMenu) {
   iconMenu.addEventListener('click', function(e) {
     document.body.classList.toggle('_lock'); //чтоб сайт за меню не скролился. В html если body._lock {overflow: hidden;}
-    iconMenu.classList.toggle('_active');
-    menuBody.classList.toggle('_active');
+    iconMenu.classList.toggle('_active');    //палочки возвращаются в бургер
+    menuBody.classList.toggle('_active');    //само тело меню закрывается
   });
 }
