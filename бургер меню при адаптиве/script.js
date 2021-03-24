6b9c2c984082ea945a9ae992e61d8d8259ff98c9
lily1215z <60219984+lily1215z@users.noreply.github.com>
2021-03-24 18:57:11 +0300

    Add files via upload

---
+++ бургер меню при адаптиве/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if(iconMenu) {
+  iconMenu.addEventListener('click', function(e) {
+    document.body.classList.toggle('_lock'); //чтоб сайт за меню не скролился. В html если body._lock {overflow: hidden;}
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
