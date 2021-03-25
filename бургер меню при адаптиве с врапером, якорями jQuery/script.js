5b9ecad95834c2c59dc24ee8b5de900dbb251d0c
lily1215z <60219984+lily1215z@users.noreply.github.com>
2021-03-25 10:48:24 +0300

    Add files via upload

---
+++ бургер меню при адаптиве с врапером, якорями jQuery/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  let links = menu.find('.burger-menu_link');
+  let overlay = menu.find('.burger-menu_overlay');
+  
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+  
+  function toggleMenu(){
+    menu.toggleClass('burger-menu_active');
+    
+    
+  }
+}
+
+burgerMenu('.burger-menu');
