cb842f25d456c5e9bcaf241907f5af1bb6d5a523
lily1215z <60219984+lily1215z@users.noreply.github.com>
2021-05-14 23:01:07 +0300

    Update script.js

--- скрыть-показывать еще блоки/script.js
+++ скрыть-показывать еще блоки/script.js
@@ -21,3 +21,34 @@ if(btnBlog) { //делала проверку т.к. код не работал 
   });
 });
 }  
+
+
+
+// 2й вариант кода. можно несколько кнопок для нажатия прописать. И в стилях просать еще код
+// .blogs__del.show {
+//   display: block;
+// }
+
+// .blogs__del,
+// .hide {
+//   display: none;
+// }
+// const more = document.querySelectorAll(".blogs__del");
+// const btnBlog = document.querySelectorAll(".blogs__btn");
+// if(btnBlog) {
+//   for(let i = 0; i < btnBlog.length; i++) {
+//     btnBlog[i].addEventListener('click', function() {
+//       more.forEach((el) => {
+//           el.classList.toggle('show');
+//         });
+
+//         if(this.classList.toggle('show')) {
+//           btnBlog[i].innerHTML = "Hidding";
+//         } else {
+//           btnBlog[i].innerHTML = "Загрузить еще 12";
+//         }
+
+//     });
+//   }
+// } 
+//классно отрабатывает на одной кнопке , но если добавляю еще кнопки то "Hidding" меняется местами - не отрабатывает как надо
